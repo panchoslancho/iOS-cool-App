c30a8fa7b9853867b17e85a6d98b58c1effd7483
Jorge Robles <jorgerobles@JR-macbookPro.local>
2016-02-08 18:17:31 -0700

    added filese for cool app

--- SCool/ViewController.swift
+++ SCool/ViewController.swift
@@ -8,16 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var BagLogo: UIImageView!
+    @IBOutlet weak var BagBG: UIImageView!
+    @IBOutlet weak var bagButton: UIButton!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func bagButtonMakeMeDoSomething(sender: AnyObject)
+    {
+    BagLogo.hidden = false
+    BagBG.hidden = false
+    bagButton.hidden = true
     }
 
 
